057ab4b77473403f1e2383d080deb67d4116c0c0
Brooke Peterson <brookepeterson08@gmail.com>
2019-01-15 16:30:19 -0500

    Create 2048.cpp

---
+++ 2048.cpp
@@ -0,0 +1,18 @@
+/*
+*	   PROJ2: 2048
+*      2048.cpp: Driver for 2048
+*      Author: Brooke Peterson
+*      Date: December 6, 2018
+*/
+
+#include <iostream>
+
+#include "game.h"
+
+using namespace std;
+
+int main() {
+    Game game;
+    game.play();
+    return 0;
+}
